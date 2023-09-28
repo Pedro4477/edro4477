627d2da70ca3791117fb5f24c9a28bef1573f16e
Pedro4477 <142502683+Pedro4477@users.noreply.github.com>
2023-09-28 09:01:08 -0300

    Create éketch.js

---
+++ éketch.js
@@ -0,0 +1,22 @@
+
+createCanvas(600, 600);
+
+background("white");
+
+}
+
+function setup() {
+
+function draw() {
+
+stroke("blue");
+
+fill("red");
+
+if(mouseIsPressed){
+
+rect(mouseX, mouseY, 20, 35);
+
+}
+
+}
